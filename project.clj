4a1fb60daa92386255ee3dcff8d0542258d45c2c
Tom Dalling <tom@tomdalling.com>
2014-06-14 21:32:30 +1000

    Working with static html, layouts, and markdown

--- project.clj
+++ project.clj
@@ -1,6 +1,14 @@
 (defproject dowlgen "0.1.0-SNAPSHOT"
   :description "Static site generator for tomdalling.com"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [me.raynes/cegdown "0.1.1"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
+                 [me.raynes/cegdown "0.1.1"]
+                 [stasis "2.1.1"]
+                 [ring "1.3.0"]
+                 [hiccup "1.0.5"]]
+  :ring {:handler dowlgen.core/app}
+  :aliases {"build-site" ["run" "-m" "dowlgen.core/export"]}
+  :profiles {
+    :dev {
+      :plugins [[lein-ring "0.8.10"]]
+      :dependencies [[midje "1.5.1"]]}}
   :main dowlgen.core)
--- src/dowlgen/core.clj
+++ src/dowlgen/core.clj
@@ -1,129 +1,54 @@
 (ns dowlgen.core
   (:require [me.raynes.cegdown :as markdown]
-            [dowlgen.string :as dstr]))
+            [clojure.java.io :as io]
+            [clojure.string :as string]
+            [hiccup.page :refer [html5]]
+            [stasis.core :as stasis]))
 
-(def path-separator java.io.File/separator)
-(def input-path ["site"])
-(def output-path ["site-out"])
+(defn map-map [key-fn val-fn coll]
+  (zipmap (map key-fn (keys coll))
+          (map val-fn (vals coll))))
 
-(defn path-split [path]
-  (dstr/split path path-separator))
+(defn map-vals [f coll]
+  (map-map identity f coll))
 
-(defn path-join [& components]
-  (->> (apply concat components)
-       (dstr/join path-separator)))
+(defn layout-page [page]
+  (html5
+    [:head
+      [:meta {:charset "utf-8"}]
+      [:meta {:name "viewport"
+              :content "width=device-width, initial-scale=1.0"}]
+      [:title "Tech blog"]
+      [:link {:rel "stylesheet" :href "/styles/styles.css"}]]
+    [:body
+      [:div.logo "cjohansen.no"]
+      [:div.body page]]))
 
-(defn path-info [path]
-  (let [components (path-split path)
-        filename (last components)
-        [base dot ext] (dstr/rpartition filename ".")]
-    {:path path
-     :dir (path-join (drop-last components))
-     :filename filename
-     :basename (if (empty? dot) filename base)
-     :extension (if (empty? dot) "" ext)}))
+(defn markdown-pages [pages]
+  (map-map #(string/replace % #"\.md$" "/")
+           #(layout-page (markdown/to-html %))
+           pages))
 
-(defn files-in [parent directory]
-  (->> (clojure.java.io/file (path-join parent directory))
-       (file-seq)
-       (filter #(.isFile %))
-       (map #(.getPath %))
-       (map #(path-split %))
-       (map #(drop (count parent) %))))
+(defn partial-pages [pages]
+  (map-vals layout-page pages))
 
-(defn -main
-  []
-  (doseq [f (files-in input-path ["blog"])]
-    (let [in-path (path-join input-path f)
-          out-path (path-join output-path f)]
-      (println input-path (path-info in-path))))
-  (println "Done."))
+(defn get-pages []
+  (stasis/merge-page-sources
+    {:public
+     (stasis/slurp-directory "resources/public" #".*\.(html|css|js)$")
+     :partials
+     (partial-pages (stasis/slurp-directory "resources/partials" #".*\.html$"))
+     :markdown
+     (markdown-pages (stasis/slurp-directory "resources/md" #".*\.md$"))}))
 
-;;;; Idea for DSL ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(def app
+  (stasis/serve-pages get-pages))
 
-; {path} = "wig/wam/boo.png"
-; {dir} = "wig/wam"
-; {basename} = "boo"
-; {extension} = "png"
-; {build-id} = "abasd24184sda" (unique per build)
+(def export-dir "dist")
 
-#_(defn group-by-tags [fs]
-    (let [tags (into #{} (mapcat fs #(attribute % "tags")))]
-      (for [t tags]
-        [t (filter #(has-tag t %) fs)])))
+(defn export []
+  (stasis/empty-directory! export-dir)
+  (stasis/export-pages (get-pages) export-dir))
 
-#_(dowlgen/site
-    {:input-dir "input/"
-     :output-dir "output/"}
-
-    ;copy files without changing them
-    (=> "static/**"
-      (files "static/**")
-
-    (=> "images/posts/{basename}_{width}x{height}.{extension}"
-      ;take every file in the `images/posts/` directory
-      (files "images/posts/*")
-      ;create two resized copies
-      (image-resize [[800 600] [640 480]])
-      ;pngcrush the two copies
-      (pngcrush))
-    
-    (=> "blog/{meta:urlname}.html"
-      ;take every `.md` file in the `blog/` directory
-      (files "blog/*.md")
-      ;take metadata out of file (allows use of {meta:*})
-      (extract-metadata)
-      ;convert from markdown to html
-      (md->html)
-      ;find and insert code snippets
-      (insert-snippets)
-      ;combine into given layout
-      (layout "_layouts/theme.html"))
-
-    (=> "js/all_{build-id}.js"
-      ;take all the coffeescript files in the `js/` dir
-      (files "js/*.coffee")
-      ;convert from coffeescript to javascript
-      (coffee->js)
-      (files "js/thirdparty/*")
-      ;combine into a single file
-      (concatenate)
-      (minify))
-
-    (=> "css/all_{build-id}.css"
-      (files "css/*.scss")
-      (scss->css)
-      (files :prepend "css/bootstrap.css")
-      (concatenate)
-      (minify))
-      
-    (=> "rss/feed.xml"
-      (files "blog/*.md")
-      (extract-metadata)
-      (sort-by #(attribute % "meta:date"))
-      (take 10)
-      (rss-feed)))
-
-    (=> "index.html"
-      (files "blog/*.md")
-      (extract-metadata)
-      (sort-by #(attribute % "meta:date"))
-      (take 2)
-      (shorten-post)
-      (layout "_layouts/post_homepage.html")
-      (concatenate)
-      (layout "_layouts/homepage.html"))
-
-    (=> "blog/category/{category}.html"
-      (files "blog/*.md")
-      (shorten-post)
-      (group-by-tags)
-      (category-page)
-      (layout "_layouts/theme.html"))
-
-    (=> "blog/archive/{year}/{month}.html"
-      (files "blog/*.md")
-      (shorten-post)
-      (group-by-month)
-      (archive-page)
-      (layout "_layouts/theme.html")))
+(defn -main []
+  (println "Hello"))
