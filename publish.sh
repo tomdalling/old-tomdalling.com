a84e6633f258a23e23cab45b9585f7c0fb9e3073
Tom Dalling <tom@tomdalling.com>
2014-08-29 20:32:37 +1000

    publish.sh

---
+++ publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eu
+
+OUTDIR="./dist/"
+TMPOUTDIR="${TMPDIR}tomdallingcom_build/"
+
+cd "$(dirname "$0")"
+rm -Rf "$OUTDIR"
+lein midje
+lein build-site
+echo "www.tomdalling.com" > "${OUTDIR}CNAME"
+echo "Build output. See 'generator' branch for source." > "${OUTDIR}README.md"
+
+rm -Rf "$TMPOUTDIR"
+mv "$OUTDIR" "$TMPOUTDIR"
+git checkout master
+rm -Rf ./*
+cp -R "${TMPOUTDIR}"* ./
+rm -Rf "$TMPOUTDIR"
+
+gitx
